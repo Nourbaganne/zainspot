ebfa3f4462d1dc23d836bf186729d01395ef8947
AmineGaf <amine.gafsi.2002@gmail.com>
2024-08-06 17:22:18 -0700

    develop subscribed services front with connexion with the backend

--- client/src/app/zainspotter/subscribed-services/page.tsx
+++ client/src/app/zainspotter/subscribed-services/page.tsx
@@ -1,22 +1,85 @@
 "use client"
 
+import { useQuery } from '@tanstack/react-query';
 import Breadcrumb from '../components/breadcrumb';
-import Layout from '../Layout'
+import Layout from '../Layout';
+import axiosInstance from '@/app/lib/axios/axiosInstance';
+import { useContext } from 'react';
+import { AuthContext } from '@/app/contexts/authContext';
+import Subscription from './component/subscription';
+
+
+export interface SubscriptionProps {
+    startDate: string,
+    endDate: string,
+    optionType: string,
+    duration: number,
+    price: number,
+    city: {
+        id: number,
+        name: string,
+        imageUrl: string,
+        locationTitle: string
+    },
+    user: {
+        email: string,
+        businessNumber: number | undefined
+    }
+}
 
 const Page = () => {
+    const { user } = useContext(AuthContext);
+
     const breadcrumbItems = [
         { label: "Breadcrumb_home", href: "/" },
         { label: "Breadcrumb_zainspotter", href: "/zainspotter" },
         { label: "editProfile_services" }
     ];
+
+    const { data, isLoading, isError, error } = useQuery({
+        queryKey: ['subscriptions'],
+        queryFn: () => axiosInstance.get(`http://localhost:3001/subscriptions/${user?.user.userId}`, {
+            headers: {
+                Authorization: `Bearer ${user?.access_token}`
+            }
+        })
+    });
+
+    if (isLoading) return <h1>Loading ...</h1>;
+    if (isError) return <h1>{error.message}</h1>;
+
+
     return (
-        <div className="flex flex-col gap-6 bg-background-foreground md:px-16 md:py-8  md:pb-20">
+        <div className="flex flex-col gap-4 md:gap-6 bg-background-foreground md:px-16 md:py-8 py-6 px-2 md:pb-20">
             <Breadcrumb items={breadcrumbItems} />
             <Layout>
-                <div>this is subscribed services view</div>
+                <div className='p-4 px-6 bg-background border pb-10 mb-20 flex flex-col gap-5'>
+                    <h1 className='font-bold'>
+                        Subscribed Services
+                    </h1>
+                    {data?.data ? (
+                        <div className='flex flex-col gap-4'>
+                            {data?.data.map((subscription: SubscriptionProps, index: number) => (
+                                <Subscription
+                                    key={index}
+                                    startDate={subscription?.startDate}
+                                    endDate={subscription?.endDate}
+                                    optionType={subscription?.optionType}
+                                    duration={subscription?.duration}
+                                    price={subscription?.price}
+                                    city={subscription?.city}
+                                    user={subscription?.user} />
+                            ))}
+                        </div>
+                    ) : (
+                        <div>
+                            no subscriptions yet
+                        </div>
+                    )}
+                </div>
             </Layout>
         </div>
-    )
+    );
 }
 
 export default Page;
