4d24742ccfd8748802c5adc53fff2b588078e100
Nour baganne <bagannenour007@gmail.com>
2024-08-18 01:36:34 +0100

    fix eslint errors

--- infrastructure/src/decorators/pagination-params.decorator.ts
+++ infrastructure/src/decorators/pagination-params.decorator.ts
@@ -1,34 +1,35 @@
-import { BadRequestException, createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { Request } from 'express';
-
+import {
+  BadRequestException,
+  createParamDecorator,
+  ExecutionContext,
+} from '@nestjs/common';
 export interface Pagination {
-    page: number;
-    limit: number;
-    size: number;
-    offset: number;
+  page: number;
+  limit: number;
+  size: number;
+  offset: number;
 }
 
-
 export const PaginationParams = createParamDecorator(
-    (data: unknown, ctx: ExecutionContext) => {
-      const request = ctx.switchToHttp().getRequest();
-      const { page = '1', limit = '6' } = request.query;
-  
-      const parsedPage = parseInt(page, 10);
-      const parsedLimit = parseInt(limit, 10);
-  
-      if (isNaN(parsedPage) || parsedPage < 1) {
-        throw new BadRequestException('Invalid page number');
-      }
-  
-      if (isNaN(parsedLimit) || parsedLimit < 1) {
-        throw new BadRequestException('Invalid limit');
-      }
-  
-      return {
-        page: parsedPage,
-        limit: parsedLimit,
-        offset: (parsedPage - 1) * parsedLimit,
-      };
-    },
-  );
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const { page = '1', limit = '6' } = request.query;
+
+    const parsedPage = parseInt(page, 10);
+    const parsedLimit = parseInt(limit, 10);
+
+    if (isNaN(parsedPage) || parsedPage < 1) {
+      throw new BadRequestException('Invalid page number');
+    }
+
+    if (isNaN(parsedLimit) || parsedLimit < 1) {
+      throw new BadRequestException('Invalid limit');
+    }
+
+    return {
+      page: parsedPage,
+      limit: parsedLimit,
+      offset: (parsedPage - 1) * parsedLimit,
+    };
+  },
+);
