6de9ab09dbfb8f7d9456aca51523fddf4f23adbf
AmineGaf <amine.gafsi.2002@gmail.com>
2024-08-18 04:41:27 -0700

    implement hiding cities

---
+++ client/src/app/lib/useHideCity.ts
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axiosInstance from '@/app/lib/axios/axiosInstance';
+import { useContext } from 'react';
+import { AuthContext } from '../contexts/authContext';
+
+export const useHideCity = () => {
+    const queryClient = useQueryClient();
+    const { user } = useContext(AuthContext);
+  
+    return useMutation({
+      mutationFn: (id: number) => {
+        return axiosInstance.patch(`/cities/${id}/hide`, null, {
+          headers: {
+            Authorization: `Bearer ${user?.access_token}`,
+          },
+        });
+      },
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ['cities'] });
+      },
+      onError: (error) => {
+        console.error('Error hiding city:', error);
+      },
+    });
+  };
+  
