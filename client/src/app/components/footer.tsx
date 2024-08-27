a54f9c2a109a07a5b332d352ecd1dd67e76977ee
Ala Baganne <alabaganne9@gmail.com>
2024-08-28 00:50:20 +0300

    Link contact page with backend api + change footer links translation way

--- client/src/app/components/footer.tsx
+++ client/src/app/components/footer.tsx
@@ -1,41 +1,39 @@
-import React from "react";
-import Image from "next/image";
-import logo from "@/app/assets/footer/zainspot.svg";
-import { FOOTER_DATA } from "@/app/constants/footer";
-import Translation from "./translation";
-import Link from "next/link";
-
+import React from 'react';
+import Image from 'next/image';
+import logo from '@/app/assets/footer/zainspot.svg';
+import { FOOTER_DATA } from '@/app/constants/footer';
+import Translation from './translation';
+import Link from 'next/link';
 
 const Footer = () => {
-
-  return (
-    <div className="flex flex-col gap-10 bg-primary text-background md:py-5 md:pt-10 md:px-20 justify-center p-6">
-      <div className="flex justify-between w-full  items-center">
-        <Image src={logo} alt="zainspot-logo" />
-        <div className="hidden md:grid grid-cols-4 gap-5 w-2/3">
-          {FOOTER_DATA.map((data, index) => (
-            <Link
-              href={data.link}
-              key={index}
-              className={`${
-                index < 4
-                  ? "font-semibold font-sans text-2xl"
-                  : "text-secondary-foreground cursor-pointer hover:underline"
-              }`}
-            >
-              <Translation translationKey={`footer_titles[${index}]`} />
-            </Link>
-          ))}
-        </div>
-      </div>
-      <div className="flex flex-col text-sm md:flex-row gap-8 text-secondary-foreground items-center font-regular">
-        <p>© 2024 ZainSpot</p>
-        <p>
-          <Translation translationKey="footer_privacy_policy" />
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div className='flex flex-col gap-10 bg-primary text-background md:py-5 md:pt-10 md:px-20 justify-center p-6'>
+			<div className='flex justify-between w-full  items-center'>
+				<Image src={logo} alt='zainspot-logo' />
+				<div className='hidden md:grid grid-cols-4 gap-5 w-2/3'>
+					{FOOTER_DATA.map((data, index) => (
+						<Link
+							href={data.link}
+							key={index}
+							className={`${
+								index < 4
+									? 'font-semibold font-sans text-2xl'
+									: 'text-secondary-foreground cursor-pointer hover:underline'
+							}`}
+						>
+							<Translation translationKey={data.translationKey} />
+						</Link>
+					))}
+				</div>
+			</div>
+			<div className='flex flex-col text-sm md:flex-row gap-8 text-secondary-foreground items-center font-regular'>
+				<p>© 2024 ZainSpot</p>
+				<p>
+					<Translation translationKey='footer_privacy_policy' />
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -23,45 +23,45 @@ import { SubscriptionModule } from './subscription/subscription.module';
 import { ContactModule } from './contact/contact.module';
 
 @Module({
-  imports: [
-    ThrottlerModule.forRoot([
-      {
-        ttl: 60000,
-        limit: 100,
-      },
-    ]),
-    ScheduleModule.forRoot(),
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validationSchema: Joi.object({
-        JWT_VERIFICATION_TOKEN_SECRET: Joi.string().required(),
-        JWT_VERIFICATION_TOKEN_EXPIRATION_TIME: Joi.string().required(),
-        EMAIL_CONFIRMATION_URL: Joi.string().required(),
-        EMAIL_SERVICE: Joi.string().required(),
-        EMAIL_USER: Joi.string().required(),
-        EMAIL_PASSWORD: Joi.string().required(),
-      }),
-    }),
-    TypeOrmModule.forRoot(dataSourceOptions),
-    AuthModule,
-    UserModule,
-    CityModule ,
-    EmailModule,
-    EmailConfirmationModule,
-    RoleModule,
-    PermissionModule,
-    PaymentHistoryModule,
-    InvoiceModule,
-    SubscriptionModule,
-    ContactModule,
-  ],
-  controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard,
-    },
-  ],
+	imports: [
+		ThrottlerModule.forRoot([
+			{
+				ttl: 60000,
+				limit: 100,
+			},
+		]),
+		ScheduleModule.forRoot(),
+		ConfigModule.forRoot({
+			isGlobal: true,
+			validationSchema: Joi.object({
+				JWT_VERIFICATION_TOKEN_SECRET: Joi.string().required(),
+				JWT_VERIFICATION_TOKEN_EXPIRATION_TIME: Joi.string().required(),
+				EMAIL_CONFIRMATION_URL: Joi.string().required(),
+				EMAIL_SERVICE: Joi.string().required(),
+				EMAIL_USER: Joi.string().required(),
+				EMAIL_PASSWORD: Joi.string().required(),
+			}),
+		}),
+		TypeOrmModule.forRoot(dataSourceOptions),
+		AuthModule,
+		UserModule,
+		CityModule,
+		EmailModule,
+		EmailConfirmationModule,
+		RoleModule,
+		PermissionModule,
+		PaymentHistoryModule,
+		InvoiceModule,
+		SubscriptionModule,
+		ContactModule,
+	],
+	controllers: [AppController],
+	providers: [
+		AppService,
+		{
+			provide: APP_GUARD,
+			useClass: ThrottlerGuard,
+		},
+	],
 })
 export class AppModule {}
