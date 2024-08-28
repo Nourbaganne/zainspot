035598206597880ed735ac6bb810760260c39b9f
Ala Baganne <alabaganne9@gmail.com>
2024-08-28 15:02:38 +0300

    Make footer fully responsive

--- client/src/app/constants/footer.ts
+++ client/src/app/constants/footer.ts
@@ -1,20 +1,36 @@
 interface FooterDataInterface {
 	translationKey: string;
-	link: string;
+	sections: { translationKey: string; link: string }[];
 }
 
 export const FOOTER_DATA: FooterDataInterface[] = [
-	{ translationKey: 'footer_title_zainspotters', link: '/zainspotter' },
-	{ translationKey: 'footer_title_about', link: '/' },
-	{ translationKey: 'footer_title_connect', link: '/' },
-	{ translationKey: 'footer_title_termsofuse', link: '/' },
-	{ translationKey: 'footer_title_join', link: '/register' },
-	{ translationKey: 'footer_title_howitworks', link: '/' },
-	{ translationKey: 'footer_title_contact', link: '/contact' },
-	{ translationKey: 'footer_title_terms', link: '/' },
-	{ translationKey: 'footer_title_login', link: '/login' },
-	{ translationKey: 'footer_title_zainspotmission', link: '/' },
-	{ translationKey: 'footer_title_faq', link: '/' },
-	{ translationKey: 'footer_title_privacypolicy', link: '/' },
-	{ translationKey: 'footer_title_gotomyzainspot', link: '/' },
+	{
+		translationKey: 'footer_title_zainspotters',
+		sections: [
+			{ translationKey: 'footer_title_join', link: '/register' },
+			{ translationKey: 'footer_title_login', link: '/login' },
+			{ translationKey: 'footer_title_gotomyzainspot', link: '/' },
+		],
+	},
+	{
+		translationKey: 'footer_title_about',
+		sections: [
+			{ translationKey: 'footer_title_howitworks', link: '/' },
+			{ translationKey: 'footer_title_zainspotmission', link: '/' },
+		],
+	},
+	{
+		translationKey: 'footer_title_connect',
+		sections: [
+			{ translationKey: 'footer_title_contact', link: '/contact' },
+			{ translationKey: 'footer_title_faq', link: '/' },
+		],
+	},
+	{
+		translationKey: 'footer_title_termsofuse',
+		sections: [
+			{ translationKey: 'footer_title_terms', link: '/' },
+			{ translationKey: 'footer_title_privacypolicy', link: '/' },
+		],
+	},
 ];
