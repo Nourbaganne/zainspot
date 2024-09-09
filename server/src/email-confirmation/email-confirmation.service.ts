c686169d13df256f185149e2702036c9d909d3eb
AmineGaf <amine.gafsi.2002@gmail.com>
2024-09-09 16:13:06 -0700

    finish the customized email

--- server/src/email-confirmation/email-confirmation.service.ts
+++ server/src/email-confirmation/email-confirmation.service.ts
@@ -24,14 +24,14 @@ export class EmailConfirmationService {
 
     const frontendUrl = `${process.env.FRONTEND_URL}/email-confirmation?token=${token}`;
     const html = `
-    <div style="background-color: #F3F3F3; width: 100%">
-      <div style="font-family: 'Work Sans', Arial, sans-serif; padding: 20px; max-width: 858px; margin: auto;">
+    <div style="background-color: #F3F3F3; width: 100%;">
+      <div style="background-color: #E8E8E8; font-family: 'Work Sans', Arial, sans-serif; padding: 20px; max-width: 858px; margin: auto;">
         <div style="text-align: center; margin-bottom: 10px; padding-top: 10px; padding-bottom: 10px;">
-          <a href="/" style="color: #727272; text-decoration: underline; margin: 0 10px;">View in the browser</a>
-          <a href="/" style="color: #727272; text-decoration: underline; margin: 0 10px;">Unsubscribe</a>
+          <a href="/" style="color: #727272; text-decoration: underline; margin: 0 2px;">View in the browser</a>
+          <a href="/" style="color: #727272; text-decoration: underline; margin: 0 2px;">Unsubscribe</a>
         </div>
         <div style="padding: 20px; background-color: #ffffff; border-radius: 8px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);">
-          <img src="https://asset.cloudinary.com/dk1upobue/b79c17024850cff8d0ce31e9252299c5" alt="ZainSpot Logo" style="width: 100%; height: auto;" />
+          <img src="cid:zainspot-logo" alt="zainspot logo"/>
           <h1 style="font-size: 18px; font-weight: 600; margin-bottom: 16px;">Confirm your email address</h1>
           <p style="font-family: 'Work Sans', Arial, sans-serif; font-weight: 400; font-size: 16px; line-height: 25.6px; color: #666666; margin: 0;">Hi there!</p>
           <p style="font-family: 'Work Sans', Arial, sans-serif; font-weight: 400; font-size: 16px; line-height: 25.6px; color: #666666; margin: 0;">We’re thrilled to have you on board at ZainSpot. Before you dive into going global with your virtual office, there’s just one small step left - verifying your email.</p>
@@ -40,15 +40,22 @@ export class EmailConfirmationService {
           <p style="font-family: 'Work Sans', Arial, sans-serif; font-weight: 400; font-size: 16px; line-height: 25.6px; color: #666666; margin-top: 20px;">If you have any questions, our support team is just an email away.</p>
           <p style="font-family: 'Work Sans', Arial, sans-serif; font-weight: 400; font-size: 16px; line-height: 25.6px; color: #666666; margin: 0;">Happy expansion,<br />The ZainSpot Team</p>
         </div>
-        <div style="background-color: #E8E8E8; padding: 10px; margin-top: 20px; border-radius: 8px;">
-          <h1 style="font-size: 16px;">ZainSpot, Inc.</h1>
-          <div style="display: flex; justify-content: space-between;">
-            <img src="https://example.com/path/to/your/logo.png" alt="Logo" style="max-width: 50px; height: auto;" />
-            <div style="display: flex; gap: 8px;">
-              <!-- Social icons can go here -->
-            </div>
-          </div>
-          <p style="color: #666666; font-size: 14px;">
+        <div style="padding: 10px; margin-top: 20px; border-radius: 8px;">
+          <table width="100%" cellpadding="0" cellspacing="0" border="0" style="margin-bottom: 10px;">
+            <tr>
+              <td style="text-align: left;">
+                <img src="cid:zainspot-logo" alt="zainspot logo"/>
+              </td>
+              <td style="text-align: right; vertical-align: middle; align-items: center;">
+                <a href=""><img src="cid:instagram-logo" alt="instagram logo" style="margin-right: 5px;"/></a>
+                <a href=""><img src="cid:facebook-logo" alt="facebook logo" style="margin-right: 5px;"/></a>
+                <a href=""><img src="cid:x-logo" alt="x logo" style="margin-right: 5px;"/></a>
+                <a href=""><img src="cid:youtube-logo" alt="youtube logo"/></a>
+              </td>
+            </tr>
+          </table>
+          <h1 style="font-size: 12px;">ZainSpot, Inc.</h1>
+          <p style="color:#B0B0B0; font-size: 12px;">
             Location,<br />
             Country
           </p>
@@ -56,7 +63,6 @@ export class EmailConfirmationService {
       </div>
     </div>
   `;
-  
 
 
     try {
@@ -64,6 +70,34 @@ export class EmailConfirmationService {
         to: email,
         subject: 'Email confirmation',
         html,
+        attachments: [{
+          "filename": "Logo_fm21jr.png",
+          "path": "https://res.cloudinary.com/dk1upobue/image/upload/v1725891603/emailVerification/Logo_fm21jr.png",
+          "cid": "zainspot-logo"
+        },
+        {
+          "filename": "instaLogo_kqhrd2.png",
+          "path": "https://res.cloudinary.com/dk1upobue/image/upload/v1725894295/emailVerification/instaLogo_kqhrd2.png",
+          "cid": "instagram-logo"
+        },
+        {
+          "filename": "facebookLogo_mq00s0.png",
+          "path": "https://res.cloudinary.com/dk1upobue/image/upload/v1725893722/emailVerification/facebookLogo_mq00s0.png",
+          "cid": "facebook-logo"
+        },
+        {
+          "filename": "XLogo_jmfnbt.png",
+          "path": "https://res.cloudinary.com/dk1upobue/image/upload/v1725893777/emailVerification/XLogo_jmfnbt.png",
+          "cid": "x-logo"
+        },
+        {
+          "filename": "youtubeLogo_yaajfa.png",
+          "path": "https://res.cloudinary.com/dk1upobue/image/upload/v1725894297/emailVerification/youtubeLogo_yaajfa.png",
+          "cid": "youtube-logo"
+        },
+
+
+        ]
       });
       this.logger.log(`Verification email sent to ${email}`);
     } catch (error) {
