c7307a6c67c3d88f8b990c4cf06a0f0a7445dda4
AmineGaf <amine.gafsi.2002@gmail.com>
2024-09-25 20:17:09 +0800

    fix role based routing bugs

--- client/src/app/components/Container.tsx
+++ client/src/app/components/Container.tsx
@@ -12,13 +12,11 @@ export default function Container({
 	children,
 	breadcrumbItems,
 	className,
-	withPaddingBottom = true, // default with padding bottom
+	withPaddingBottom = true,
 }: Props) {
 	return (
-		<div
-			className={
-				'bg-gray-100 ' + (withPaddingBottom ? 'pb-12 ' : '') + className
-			}
+		<div className={
+				'bg-gray-100 ' + (withPaddingBottom ? 'pb-12 ' : '') + className}
 		>
 			{breadcrumbItems && (
 				<Breadcrumb items={breadcrumbItems} className='p-6' />
--- client/src/app/lib/withAuth.tsx
+++ client/src/app/lib/withAuth.tsx
@@ -1,59 +1,62 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { AuthContext } from "../contexts/authContext";
 import Loader from "../components/loader";
 
 export function WithAuth<P extends object>(
-  WrappedComponent: React.ComponentType<P>, 
+  WrappedComponent: React.ComponentType<P>,
   requiredRole?: string
 ) {
   return function WithAuthComponent(props: P) {
-    const { user, loading, dispatch } = useContext(AuthContext);
+    const { user, dispatch } = useContext(AuthContext);
     const router = useRouter();
+    const [isAuthorized, setIsAuthorized] = useState<boolean | null>(null); 
 
-    // Helper function to fully log out the user
+    // Helper function to log out the user
     const logoutUser = () => {
-      localStorage.removeItem('token');   // Remove the token from localStorage
-      localStorage.removeItem('user');    // Remove any stored user data
-      dispatch({ type: 'LOGOUT' });       // Clear user data from context
-      router.push('/login');              // Redirect to login page
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      dispatch({ type: 'LOGOUT' });
+      router.push('/login');
     };
 
     useEffect(() => {
-      if (loading) return; // Wait until loading is complete
-
       // Check expiration
       if (user?.expires_at) {
-        const expires_at = new Date(user.expires_at);
-        const current_time = new Date();
+        const expiresAt = new Date(user.expires_at);
+        const currentTime = new Date();
 
-        if (expires_at <= current_time) {
-          // Token has expired, log the user out
+        if (expiresAt <= currentTime) {
           logoutUser();
           return;
         }
       }
 
-      // If no user, log them out
+      // If no user, redirect to login
       if (!user) {
-        router.push('/login')
+        router.push('/login');
         return;
       }
 
       // Role-based authorization
       if (requiredRole && user.user.role.name !== requiredRole) {
+        setIsAuthorized(false); 
         if (user.user.role.name === 'admin') {
           router.push('/unauthorized');
         } else {
           router.push('/');
         }
+        return; // Prevent rendering if redirected
       }
-    }, [user, loading, router, requiredRole]);
 
-    if (loading) {
-      return <Loader />;
+      setIsAuthorized(true); // Set authorized state
+    }, [user, router, requiredRole]);
+
+    // Render the wrapped component if authorized
+    if (isAuthorized) {
+      return <WrappedComponent {...props} />;
     }
 
-    return <WrappedComponent {...props} />;
+    return <Loader />; // Prevent rendering if unauthorized
   };
 }
