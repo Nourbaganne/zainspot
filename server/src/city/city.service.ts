1038226eae7899b18af3c4b4ba7080f49fd7f735
Ala Baganne <alabaganne9@gmail.com>
2024-10-16 18:05:53 +0100

    Automatically assign stripePriceId for each price after city create/edit

--- server/src/city/city.service.ts
+++ server/src/city/city.service.ts
@@ -7,12 +7,15 @@ import { City } from 'src/entities/city.entity';
 import cloudinary from 'src/config/cloudinary.config';
 import { Pagination } from 'src/decorators/pagination-params.decorator';
 import { PaginatedResource } from 'src/decorators/dto/paginated-resources.dto';
+import { StripeService } from 'src/stripe/stripe.service';
+import PerMonth from 'src/interfaces/PerMonth';
 @Injectable()
 export class CityService {
 	constructor(
 		@InjectRepository(City)
 		private cityRepository: Repository<City>,
-	) { }
+		private stripeService: StripeService,
+	) {}
 
 	async getCities(
 		{ page, limit, size, offset, hidden }: Pagination,
@@ -38,7 +41,8 @@ export class CityService {
 		const totalLocations = await queryBuilder.getCount();
 
 		// Get total number of distinct cities after filters
-		const totalCitiesQuery = this.cityRepository.createQueryBuilder('city')
+		const totalCitiesQuery = this.cityRepository
+			.createQueryBuilder('city')
 			.select('COUNT(DISTINCT city.city)', 'count');
 
 		if (name) {
@@ -52,11 +56,14 @@ export class CityService {
 		const totalCities = await totalCitiesQuery.getRawOne();
 
 		// Get total number of distinct countries after filters
-		const totalCountriesQuery = this.cityRepository.createQueryBuilder('city')
+		const totalCountriesQuery = this.cityRepository
+			.createQueryBuilder('city')
 			.select('COUNT(DISTINCT city.country)', 'count');
 
 		if (name) {
-			totalCountriesQuery.andWhere('city.city LIKE :name', { name: `%${name}%` });
+			totalCountriesQuery.andWhere('city.city LIKE :name', {
+				name: `%${name}%`,
+			});
 		}
 
 		if (hidden !== undefined) {
@@ -87,7 +94,6 @@ export class CityService {
 		};
 	}
 
-
 	async getCity(id: number): Promise<City> {
 		const city = await this.cityRepository.findOne({ where: { id } });
 		if (!city) {
@@ -113,6 +119,56 @@ export class CityService {
 		});
 	}
 
+	private async assignStripePriceIdToEachCityPrice(
+		city: CreateCityDto,
+	): Promise<CreateCityDto> {
+		if (city.goldPrice) {
+			const gp =
+				typeof city.goldPrice === 'string'
+					? JSON.parse(city.goldPrice)
+					: city.goldPrice;
+			console.log('gp', gp);
+			const goldPrice = await this.stripeService.stripe.prices.create({
+				currency: 'usd',
+				unit_amount: gp.amount,
+				recurring: {
+					interval: 'month',
+					interval_count: gp.duration,
+				},
+				product_data: { name: `${city.city} Gold Membership` },
+			});
+			gp.stripePriceId = goldPrice.id;
+			city.goldPrice =
+				typeof city.goldPrice === 'string' ? JSON.stringify(gp) : gp;
+		}
+		if (city.classicPrice) {
+			const cp =
+				typeof city.classicPrice === 'string'
+					? JSON.parse(city.classicPrice)
+					: city.classicPrice;
+			cp.perMonth = await Promise.all(
+				cp.perMonth
+					.filter((price: PerMonth) => price.amount && price.amount != 0)
+					.map(async (price: PerMonth) => {
+						console.log('price', price);
+						const classicPrice = await this.stripeService.stripe.prices.create({
+							currency: 'usd',
+							unit_amount: price.amount,
+							recurring: { interval: 'month', interval_count: price.duration },
+							product_data: { name: `${city.city} Classic Membership` },
+						});
+						return { ...price, stripePriceId: classicPrice.id };
+					}),
+			);
+
+			city.classicPrice =
+				typeof city.classicPrice === 'string' ? JSON.stringify(cp) : cp;
+		}
+
+		console.log('city', city);
+		return city;
+	}
+
 	async createCity(
 		createCityDto: CreateCityDto,
 		file: Express.Multer.File,
@@ -121,8 +177,70 @@ export class CityService {
 			const imageUrl = await this.uploadImageToCloudinary(file);
 			createCityDto.imageUrl = imageUrl;
 		}
+		createCityDto =
+			await this.assignStripePriceIdToEachCityPrice(createCityDto);
 		const newCity = this.cityRepository.create(createCityDto);
+		console.log('new city', newCity);
 		return await this.cityRepository.save(newCity);
+	}
+
+	private async checkForUpdatedPrices(
+		updateCityDto: UpdateCityDto,
+		oldCity: City,
+	): Promise<UpdateCityDto> {
+		const gp =
+			typeof updateCityDto.goldPrice === 'string'
+				? JSON.parse(updateCityDto.goldPrice)
+				: updateCityDto.goldPrice;
+		const cp =
+			typeof updateCityDto.classicPrice === 'string'
+				? JSON.parse(updateCityDto.classicPrice)
+				: updateCityDto.classicPrice;
+
+		if (oldCity.goldPrice.amount != gp.amount) {
+			console.log('Creating new price for gold membership for', oldCity.city);
+			const goldPrice = await this.stripeService.stripe.prices.create({
+				currency: 'usd',
+				unit_amount: gp.amount,
+				recurring: {
+					interval: 'month',
+					interval_count: gp.duration,
+				},
+				product_data: { name: `${oldCity.city} Gold Membership` },
+			});
+			gp.stripePriceId = goldPrice.id;
+			updateCityDto.goldPrice =
+				typeof updateCityDto.goldPrice === 'string' ? JSON.stringify(gp) : gp;
+		}
+		console.log('cp', cp);
+		cp.perMonth = await Promise.all(
+			cp.perMonth.map(async (newPrice: PerMonth) => {
+				const oldPrice = oldCity.classicPrice.perMonth.find(
+					(price: PerMonth) => price.duration === newPrice.duration,
+				) || { amount: 0, duration: newPrice.duration, tax: newPrice.tax };
+				if (newPrice.amount == 0) {
+					newPrice.stripePriceId = null;
+				} else if (oldPrice.amount != newPrice.amount) {
+					console.log(
+						'Creating new price for classic membership for',
+						oldCity.city,
+					);
+					const classicPrice = await this.stripeService.stripe.prices.create({
+						currency: 'usd',
+						unit_amount: newPrice.amount,
+						recurring: { interval: 'month', interval_count: newPrice.duration },
+						product_data: { name: `${oldCity.city} Classic Membership` },
+					});
+					newPrice.stripePriceId = classicPrice.id;
+				}
+				return newPrice;
+			}),
+		);
+
+		updateCityDto.classicPrice =
+			typeof updateCityDto.classicPrice === 'string' ? JSON.stringify(cp) : cp;
+
+		return updateCityDto;
 	}
 
 	async updateCity(
@@ -134,6 +252,14 @@ export class CityService {
 			const imageUrl = await this.uploadImageToCloudinary(file);
 			updateCityDto.imageUrl = imageUrl;
 		}
+
+		const oldCity = await this.cityRepository.findOne({ where: { id } });
+		if (!oldCity) {
+			throw new NotFoundException('City not found');
+		}
+
+		updateCityDto = await this.checkForUpdatedPrices(updateCityDto, oldCity);
+
 		await this.cityRepository.update(id, updateCityDto);
 		const updatedCity = await this.cityRepository.findOne({ where: { id } });
 		if (!updatedCity) {
