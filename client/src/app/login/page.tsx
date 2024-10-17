e76fe0a2a02247bd520bb2c3629587242a5dc223
AmineGaf <amine.gafsi.2002@gmail.com>
2024-10-17 15:11:58 +0100

    implement private routes to login & register

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import { Input } from '../register/components/input';
 import { useLoginForm } from '../lib/login-form';
 import Link from 'next/link';
+import { withNoAuth } from '../lib/withNoAuth';
 import InputPassword from '../components/inputPassword';
 import WelcomeToBusinessSection from '../components/WelcomeToBusiness';
 import ResetPasswordDialog from '../components/resetPasswordDialog';
@@ -88,4 +89,4 @@ const Page = () => {
 	);
 };
 
-export default Page;
+export default withNoAuth(Page);
