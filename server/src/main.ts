5995512037f54ed703d4cf66d705ada9a4c4a2e2
Nourbaganne <bagannenour007@gmail.com>
2024-10-22 15:12:37 +0100

    update main.ts for testing purposes

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,19 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as bodyParser from 'body-parser';
-import { join } from 'path';
-import { existsSync, mkdirSync } from 'fs';
+// import * as bodyParser from 'body-parser';
+// import { join } from 'path';
+// import { existsSync, mkdirSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const uploadPath = join(__dirname, '..', 'uploads', 'users');
-  if (!existsSync(uploadPath)) {
-    mkdirSync(uploadPath, { recursive: true });
-  }
+  // const uploadPath = join(__dirname, '..', 'uploads', 'users');
+  // if (!existsSync(uploadPath)) {
+  //   mkdirSync(uploadPath, { recursive: true });
+  // }
+  // app.use(bodyParser.json({ limit: '20mb' }));
+  // app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
 
-  app.use(bodyParser.json({ limit: '20mb' }));
-  app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
   app.enableCors();
   console.log('Server running on http://localhost:3001');
   await app.listen(3001);
