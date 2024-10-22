f6839b4aa57d9d5c42481391e642ee763511e483
Nourbaganne <bagannenour007@gmail.com>
2024-10-22 16:04:35 +0100

    remove multer

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -5,32 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { EmailConfirmationModule } from '../email-confirmation/email-confirmation.module';
 import { RecaptchaService } from './recaptcha.service';
-import { MulterModule } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     forwardRef(() => EmailConfirmationModule),
-    MulterModule.register({
-      storage: diskStorage({
-        destination: '../../uploads/users',
-        filename: (req, file, cb) => {
-          // Generate a unique filename
-          const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-          const extension = file.originalname.split('.').pop();
-          cb(null, `${uniqueSuffix}.${extension}`);
-        },
-      }),
-      limits: { fileSize: 5 * 1024 * 1024 }, 
-      fileFilter: (req, file, cb) => {
-        if (!file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
-          cb(new Error('Unsupported file type'), false);
-        } else {
-          cb(null, true);
-        }
-      },
-    }),
   ],
   providers: [UserService, RecaptchaService],
   exports: [UserService],
