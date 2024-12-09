0d98912f151a55f65c5943de59938caaa0cae805
Nourbaganne <bagannenour007@gmail.com>
2024-12-09 01:22:28 +0100

    fix text weight

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -75,7 +75,7 @@ function Home() {
 			</div>
 
 			<div className='flex flex-col py-12 md:px-14 px-5 gap-9 '>
-				<p className='font-sans font-semibold text-text-foreground text-xl md:text-2xl '>
+				<p className='font-sans font-[590px] text-text-foreground text-xl md:text-2xl '>
 					<Translation translationKey='homepage_cities_description' />
 				</p>
 				<div className='flex flex-col md:pl-12 gap-5'>
