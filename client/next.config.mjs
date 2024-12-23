aef52490a7bac35414e934ada0e50d63163e98d5
Yassin bagane <yassin.bagane@datavid.com>
2024-12-23 18:44:49 +0100

    Specify dist dir

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['res.cloudinary.com'],
+		domains: ['prod.zainspot.com/'],
 	},
 	experimental: {
 		missingSuspenseWithCSRBailout: false,
 	},
 	reactStrictMode: false,
+	distDir: 'client/.next'
 };
 
 export default nextConfig;
