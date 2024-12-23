4267e31cffb165553d5168d6190cb3ac8fafd925
Nourbaganne <bagannenour007@gmail.com>
2024-12-23 17:07:32 +0100

    commit

--- client/src/app/lib/useUserStats.ts
+++ client/src/app/lib/useUserStats.ts
@@ -8,7 +8,7 @@ export const UseUSerStats = () => {
     const { user } = useContext(AuthContext);
     const { data } = useQuery({
         queryKey: ['userStats'],
-        queryFn: async () => axiosInstance.get('http://localhost:3001/user/userStats', {
+        queryFn: async () => axiosInstance.get('/user/userStats', {
             headers: {
                 Authorization: `Bearer ${user?.access_token}`,
             },
