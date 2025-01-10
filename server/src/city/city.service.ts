11e001e5d1877b09438345cde676ee695e1f7269
AmineGaf <amine.gafsi.2002@gmail.com>
2025-01-10 21:10:16 +0100

    translations updates

--- server/src/city/city.service.ts
+++ server/src/city/city.service.ts
@@ -106,6 +106,10 @@ export class CityService {
 			throw new NotFoundException('City not found');
 		}
 
+		if (!lang || lang === 'usa') {
+			lang = 'en';
+		}
+
 		const translatedCity = {
 			...city,
 			description: await this.translationService.translateText(city.description, 'en', lang),
